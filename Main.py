28f3c82006b78ca0cb7240b18176430ef8efeb3b
Francesco Mecatti <francesco.mecatti@github.com>
2019-12-29 23:23:45 +0100

    Created module for file language recognition

--- Main.py
+++ Main.py
@@ -4,14 +4,7 @@ from Modules.analyzer import LettersFreq, FileLingua
 
 
 def main():
-    LettersFreq.set_file("Frequency_Tables/letters_frequency.csv")
-    ita = FileLingua("Test_Files/ita.txt")
-    print(ita)
-    ita.save_stat_file()
 
-    fr = FileLingua("Test_Files/fr.txt")
-    print(fr)
-    fr.save_stat_file()
 
 
 if __name__ == "__main__":
--- Modules/analyzer.py
+++ Modules/analyzer.py
@@ -1,3 +1,7 @@
+#!/bin/python
+
+#This module provides classes and constants to analyze the language of a text given as input (also form a file)
+
 import csv
 import math
 import string
@@ -18,14 +22,14 @@ class LettersFreq:
         with open(LettersFreq.filename, "r") as file:
             csv_file = csv.DictReader(file, delimiter=',')
             for row in csv_file:
-                row = dict(row) #Convert row to dict
+                row = dict(row)  # Convert row to dict
                 lang = row['Language']
-                del row['Language'] #Remove 'Language' fields
+                del row['Language']  # Remove 'Language' fields
                 for letter, freq in row.items():
-                    row[letter] = float(freq.strip("%")) #Convert form num% to float
-                #print(row)
-                LettersFreq.letters_freq[lang] = row #Remain only a dict of letter:frequency
-            #print(LettersFreq.letters_freq)
+                    row[letter] = float(freq.strip("%"))  # Convert form num% to float
+                # print(row)
+                LettersFreq.letters_freq[lang] = row  # Remain only a dict of letter:frequency
+            # print(LettersFreq.letters_freq)
 
 
 class FileLingua(LettersFreq):
@@ -36,20 +40,20 @@ class FileLingua(LettersFreq):
         self.rows = len(self.txt.split('\n')) - 1
         self.words = len(self.txt.split())
         self.chars = len(self.txt)
-        self.chars_freq = {} #Frequency of each alphabet char
-        self.lang = '' #Language
-        self.distances = {} #Distance between file chars freq and refernce chars freq
+        self.chars_freq = {}  # Frequency of each alphabet char
+        self.lang = ''  # Language
+        self.distances = {}  # Distance between file chars freq and refernce chars freq
         file.close()
         self._extract_chars_freq()
         self._find_lang()
 
     def __repr__(self):  # Overriding object method
         return "\"%s\" have %d chars, %d words, %d rows. Written in %s language." % (
-        self.filename, self.chars, self.words, self.rows, self.lang)
+            self.filename, self.chars, self.words, self.rows, self.lang)
 
     def __str__(self):
         return "\"%s\" [chars=%d, words=%d, rows=%d, language=%s]" % (
-        self.filename, self.chars, self.words, self.rows, self.lang)
+            self.filename, self.chars, self.words, self.rows, self.lang)
 
     def file_format(self):
         chars_freq_scheme = ''
@@ -68,20 +72,20 @@ Language: {}
     def _extract_chars_freq(self):  # Private method
         if (not self.chars_freq):  # Check if chars_freq dict already exists
             for letter in charset:  # if not pupulate it
-                self.chars_freq[letter] = (self.txt.count(letter) *100 / self.chars)
+                self.chars_freq[letter] = (self.txt.count(letter) * 100 / self.chars)
 
     def _find_lang(self):
         distances = {}
         for lang, freq_dict in LettersFreq.letters_freq.items():
             dist = 0
             for (letter, freq1), (letter, freq2) in zip(freq_dict.items(), self.chars_freq.items()):
-                dist += (freq1-freq2)**2
+                dist += (freq1 - freq2) ** 2
             distances[math.sqrt(dist)] = lang
-        #print(distances) #Debug
+        # print(distances) #Debug
         self.lang = distances[min(distances.keys())]
 
     def save_stat_file(self):
-        filename = self.filename.split('.')[0] + ".stat"
+        filename = self.filename.rsplit('.', 1)[0] + ".stat"  # split starting from the right
         file = open(filename, "wt")
         file.write(self.file_format())
         file.close()
@@ -95,3 +99,18 @@ Language: {}
     def evaluate_quality(self):
         '''implements Twitter API to estimate a quality of reconaissance'''
         pass
+
+
+def main():
+    LettersFreq.set_file("../Frequency_Tables/letters_frequency.csv")
+    ita = FileLingua("../Test_Files/ita.txt")
+    print(ita)
+    ita.save_stat_file()
+
+    fr = FileLingua("../Test_Files/fr.txt")
+    print(fr)
+    fr.save_stat_file()
+
+
+if __name__ == "__main__":
+    main()
